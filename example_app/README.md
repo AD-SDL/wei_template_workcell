569e9dc99c8fea2c6ae2579cafa34e778ec24c08
Ryan Lewis <accounts+github@ryandlewis.dev>
2024-02-16 16:13:33 -0600

    Purge makefile, simplify and document

--- example_app/README.md
+++ example_app/README.md
@@ -1,1 +1,11 @@
 # Example App
+
+In the `example_app` directory:
+
+```
+python -m venv .venv
+source .venv/bin/activate
+pip install -e .
+# After starting the workcell:
+python example_app.py
+```
--- example_app/example_app.py
+++ example_app/example_app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Runs an example of a WEI workflow"""
+"""Example experiment application that uses the WEI client to run a workflow."""
 
 import json
 from pathlib import Path
@@ -9,11 +9,11 @@ from wei import ExperimentClient
 
 def main() -> None:
     """Runs an example WEI workflow"""
+    # This defines the Experiment object that will communicate with the WEI server
+    exp = ExperimentClient("localhost", "8000", "Example_Program")
+
     # The path to the Workflow definition yaml file
     wf_path = Path(__file__).parent / "workflows" / "example_workflow.yaml"
-
-    # This defines the Experiment object that will communicate with the WEI server
-    exp = ExperimentClient("wei_server", "8000", "Example_Program")
 
     # This runs the workflow
     flow_info = exp.start_run(
@@ -28,7 +28,7 @@ def main() -> None:
     # The below line can be used to fetch the result and save it in our local directory
     exp.get_file(
         flow_info["hist"]["Take Picture"]["action_msg"],
-        "/home/app/.wei/experiment_output.jpg",
+        "experiment_output.jpg",
     )
 
 
